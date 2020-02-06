83b7f671b996c2345d4e6a4571d9f9bb12dd17bd
Alexandr Fedchenko <a.fedchenko@n1.ru>
2020-02-06 19:56:11 +0700

    Немного продвинулся

--- app/src/main/java/com/example/googlekeeperkotlin/data/Repository.kt
+++ app/src/main/java/com/example/googlekeeperkotlin/data/Repository.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 object Repository {
 
-    val notes: MutableList<Note> = mutableListOf(
+    private val notes: MutableList<Note> = mutableListOf(
         Note(id = UUID.randomUUID().toString(),
             title = "Моя первая заметка",
             note = "Kotlin очень краткий, но при этом выразительный язык",
